770dad210502fb71ab880396316ae5418aeabe65
Buildbot_Jignesh <jigneshshukla212@gmail.com>
2024-12-04 20:33:00 +0530

    build derpfest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,14 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_everpal.mk
+    $(LOCAL_DIR)/spark_everpal.mk \
+    $(LOCAL_DIR)/derp_everpal.mk
+
 
 COMMON_LUNCH_CHOICES := \
     spark_everpal-user \
     spark_everpal-userdebug \
-    spark_everpal-eng
+    spark_everpal-eng \
+    derp_everpal-user \
+    derp_everpal-userdebug \
+    derp_everpal-eng
